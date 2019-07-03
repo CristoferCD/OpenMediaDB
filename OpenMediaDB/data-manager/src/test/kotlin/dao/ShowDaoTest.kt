01a4192cf9681b8316f7d063b25d9446ace92f6a
Cristofer <cristofercnsdmngz@live.com>
2019-07-03 19:24:51 +0200

    Testcontainers

--- OpenMediaDB/data-manager/src/test/kotlin/dao/ShowDaoTest.kt
+++ OpenMediaDB/data-manager/src/test/kotlin/dao/ShowDaoTest.kt
@@ -1,7 +1,14 @@
 package dao
 
 import DataManagerFactory
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.Test
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.images.builder.ImageFromDockerfile
+import java.nio.file.Paths
+import java.util.concurrent.Future
+import java.util.concurrent.TimeUnit
 
 internal class ShowDaoTest {
 
@@ -13,4 +20,23 @@ internal class ShowDaoTest {
         println(result)
         assert(true)
     }
+
+    @Test
+    fun container() {
+        val container = KGenericContainer("local/omediapostgres")
+
+        container.start()
+
+        val db = Database.connect("jdbc:postgresql://${container.containerIpAddress}:${container.getMappedPort(5432)}/postgres", user = "omediatest", password = "1234", driver = "org.postgresql.Driver")
+
+        transaction(db) {
+            exec("Select * from 'Show'") { rs ->
+                while (rs.next()) {
+                    println(rs.toString())
+                }
+            }
+        }
+    }
 }
+
+class KGenericContainer(image: String): GenericContainer<KGenericContainer>(image)
