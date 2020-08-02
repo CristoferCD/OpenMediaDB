a16de130ce8915c5565a1a54b4c12ebf954c9202
CristoferCD <cristofer_cd@outlook.es>
2020-08-02 19:18:51 +0200

    Stream video in 1MB chunks

--- OpenMediaDB/open-media-db-parent/src/main/kotlin/app/controller/VideoController.kt
+++ OpenMediaDB/open-media-db-parent/src/main/kotlin/app/controller/VideoController.kt
@@ -3,16 +3,16 @@ package app.controller
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.core.io.FileSystemResource
+import org.springframework.core.io.support.ResourceRegion
+import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/video")
 @Tag(name = "Videos")
 internal interface VideoController {
     @GetMapping("/{token}", produces = [MediaType.APPLICATION_OCTET_STREAM_VALUE])
     @Operation(summary = "Get video stream")
-    fun streamVideo(@PathVariable token: String): FileSystemResource
+    fun streamVideo(@PathVariable token: String, @RequestHeader headers: HttpHeaders): ResponseEntity<ResourceRegion>
 }
--- OpenMediaDB/open-media-db-parent/src/main/kotlin/app/controller/impl/VideoControllerImpl.kt
+++ OpenMediaDB/open-media-db-parent/src/main/kotlin/app/controller/impl/VideoControllerImpl.kt
@@ -3,13 +3,32 @@ package app.controller.impl
 import app.controller.BaseController
 import app.controller.VideoController
 import org.springframework.core.io.FileSystemResource
+import org.springframework.core.io.support.ResourceRegion
+import org.springframework.http.*
 import org.springframework.web.bind.annotation.RestController
+import kotlin.math.min
 
 @RestController
 internal class VideoControllerImpl : VideoController, BaseController() {
-    override fun streamVideo(token: String): FileSystemResource {
+    override fun streamVideo(token: String, headers: HttpHeaders): ResponseEntity<ResourceRegion> {
         val videoToken = dataManagerFactory.tokenDao.get(token)!!
         val file = dataManagerFactory.fileInfoDao.get(videoToken.fileId)!!
-        return FileSystemResource(file.path)
+        val video = FileSystemResource(file.path)
+        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+                .contentType(MediaTypeFactory.getMediaType(video).orElse(MediaType.APPLICATION_OCTET_STREAM))
+                .body(resourceRegion(video, headers.range.firstOrNull()))
+    }
+
+    private fun resourceRegion(video: FileSystemResource, range: HttpRange?): ResourceRegion {
+        val contentLength = video.contentLength()
+        return if (range != null) {
+            val start = range.getRangeStart(contentLength)
+            val end = range.getRangeEnd(contentLength)
+            val length = min(1000000L, end - start + 1)
+            ResourceRegion(video, start, length)
+        } else {
+            val length = min(1000000L, contentLength)
+            ResourceRegion(video, 0, length)
+        }
     }
 }
