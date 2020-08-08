2f96a20d99ac80e6ca9689c37f207d0e5cb88e14
CristoferCD <cristofer_cd@outlook.es>
2020-08-08 14:38:50 +0200

    Throw error if token expired

--- OpenMediaDB/open-media-db-parent/src/main/kotlin/app/controller/impl/VideoControllerImpl.kt
+++ OpenMediaDB/open-media-db-parent/src/main/kotlin/app/controller/impl/VideoControllerImpl.kt
@@ -6,12 +6,18 @@ import org.springframework.core.io.FileSystemResource
 import org.springframework.core.io.support.ResourceRegion
 import org.springframework.http.*
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+import java.time.LocalDateTime
+import java.time.ZoneId
 import kotlin.math.min
 
 @RestController
 internal class VideoControllerImpl : VideoController, BaseController() {
     override fun streamVideo(token: String, headers: HttpHeaders): ResponseEntity<ResourceRegion> {
-        val videoToken = dataManagerFactory.tokenDao.get(token)!!
+        val videoToken = dataManagerFactory.tokenDao.get(token)
+        if (videoToken == null || videoToken.expires.isBefore(LocalDateTime.now().atZone(ZoneId.systemDefault()))) {
+            throw ResponseStatusException(HttpStatus.GONE, "Token expired")
+        }
         val file = dataManagerFactory.fileInfoDao.get(videoToken.fileId)!!
         val video = FileSystemResource(file.path)
         return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
@@ -20,14 +26,15 @@ internal class VideoControllerImpl : VideoController, BaseController() {
     }
 
     private fun resourceRegion(video: FileSystemResource, range: HttpRange?): ResourceRegion {
+        val chunkSize = 1000000L
         val contentLength = video.contentLength()
         return if (range != null) {
             val start = range.getRangeStart(contentLength)
             val end = range.getRangeEnd(contentLength)
-            val length = min(1000000L, end - start + 1)
+            val length = min(chunkSize, end - start + 1)
             ResourceRegion(video, start, length)
         } else {
-            val length = min(1000000L, contentLength)
+            val length = min(chunkSize, contentLength)
             ResourceRegion(video, 0, length)
         }
     }
